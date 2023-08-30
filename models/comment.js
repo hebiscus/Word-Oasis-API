6253420501ade11bf6935629e786bb7e08a7247b
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-08-30 13:48:35 +0200

    fix typo in comment model

--- models/comment.js
+++ models/comment.js
@@ -4,7 +4,7 @@ const commentSchema = mongoose.Schema({
     blogpost: {type: mongoose.Schema.ObjectId, ref: "BlogPost", required: true},
     author: {type: String, minlength: 1, maxlength: 30, required: true},
     content: {type: String, minlength: 3, maxlength: 300, required: true},
-    reationDate: {type: Date, default: Date.now, required: true},
+    creationDate: {type: Date, default: Date.now, required: true},
 });
 
 module.exports = mongoose.model("Comment", commentSchema);
